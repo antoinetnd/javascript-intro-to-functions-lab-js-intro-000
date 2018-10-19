59a91e431f13336ebe92c60324ef9077afe5162f
Antoine Tournedouet <antoine.tournedouet@gmail.com>
2018-10-19 19:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function whisper(string){
 function logWhisper (string){
   console.log(whisper(string));
 }
-
+function logShout(string){console.log(shout(string));}
 function sayHiToGrandma (string){
   if (string == shout(string)){
     return "YES INDEED!";
